c8733aff368cf322d868cccf737ea6ba509ff549
Marc Ciejay Macaraig <macaraigmarcciejay@gmail.com>
2025-01-18 22:58:55 +0800

    Version 1.4

---
+++ app.js
@@ -0,0 +1,47 @@
+import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
+import { gsap } from 'https://cdn.skypack.dev/gsap';
+
+const camera = new THREE.PerspectiveCamera(
+    10,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+camera.position.z = 13;
+
+const scene = new THREE.Scene();
+let bee;
+let mixer;
+const loader = new GLTFLoader();
+loader.load('/resourcess/panasonic_nv-gs5en_dv-camcordercamera.glb',
+    function (gltf) {
+        bee = gltf.scene;
+        scene.add(bee);
+
+        mixer = new THREE.AnimationMixer(bee);
+        mixer.clipAction(gltf.animations[0]).play();
+        modelMove();
+    },
+    function (xhr) {},
+    function (error) {}
+);
+const renderer = new THREE.WebGLRenderer({alpha: true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById('container3D').appendChild(renderer.domElement);
+
+// light
+const ambientLight = new THREE.AmbientLight(0xffffff, 1.3);
+scene.add(ambientLight);
+
+const topLight = new THREE.DirectionalLight(0xffffff, 1);
+topLight.position.set(500, 500, 500);
+scene.add(topLight);
+
+
+const reRender3D = () => {
+    requestAnimationFrame(reRender3D);
+    renderer.render(scene, camera);
+    if(mixer) mixer.update(0.02);
+};
+reRender3D();
