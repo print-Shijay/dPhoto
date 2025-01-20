32d3264b7f26f50f2a4950050b146ac52268572d
Marc Ciejay Macaraig <macaraigmarcciejay@gmail.com>
2025-01-20 19:40:04 +0800

    Version 1.8.1

--- parallax.js
+++ parallax.js
@@ -15,3 +15,65 @@ window.addEventListener('scroll', ()=> {
     sand.style.left = value * 1.5 + 'px';
 
 });
+
+const banner = document.querySelector('.content');
+const infoSection = document.querySelector('.info');
+const parallaxSection = document.querySelector('.parallax');
+
+const percentageVisible = 0.15;
+const percentagePosition = 0.74;
+
+window.addEventListener('scroll', () => {
+  const infoSectionTop = infoSection.getBoundingClientRect().top;
+  const infoSectionHeight = infoSection.offsetHeight;
+  const parallaxSectionTop = parallaxSection.getBoundingClientRect().top;
+  const parallaxSectionHeight = parallaxSection.offsetHeight;
+
+  if (infoSectionTop + infoSectionHeight * percentageVisible <= window.innerHeight && infoSectionTop + infoSectionHeight * percentageVisible >= 0) {
+    const opacity = Math.min(1, (window.innerHeight - (infoSectionTop + infoSectionHeight * percentageVisible)) / (window.innerHeight / 2));
+    banner.style.opacity = opacity;
+  } else if (parallaxSectionTop <= window.innerHeight && parallaxSectionTop + parallaxSectionHeight > 0) {
+    banner.style.opacity = 0;
+  }
+
+  if (infoSectionTop + infoSectionHeight * percentagePosition > window.innerHeight) {
+    banner.style.position = 'fixed';
+    banner.style.transition = 'top 0.5s ease-in-out';
+  }
+
+  if (infoSectionTop + infoSectionHeight * percentagePosition <= window.innerHeight && infoSectionTop + infoSectionHeight * percentagePosition >= 0) {
+    banner.style.position = 'absolute';
+    banner.style.transition = 'top 0.5s ease-in-out';
+  }
+
+  if (!(parallaxSectionTop <= window.innerHeight && parallaxSectionTop + parallaxSectionHeight > 0)) {
+    banner.style.opacity = 1;
+  }
+});
+
+
+let sections = document.querySelectorAll('div');
+
+window.onscroll = () => {
+  sections.forEach(sec =>{
+    let top = window.scrollY;
+    let offset = sec.offsetTop +500;
+    let height = sec.offsetHeight+1000;
+
+    if(top >= offset && top < offset + height){
+      sec.classList.add('show-animate');
+    }
+
+    else{
+      sec.classList.remove('show-animate');
+    }
+  })
+}
+
+
+
+
+
+
+
+
